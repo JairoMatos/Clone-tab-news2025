2f1c1b6ebf05325bc5270a76b91b00b5904efe94
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2025-11-15 22:36:50 +0000

    altera frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
+
 function Home(){
   return(
-    <h1>Te amo muito meu <strong>amor</strong>, voce e a pessoa mais linda do mundo!</h1>
+    <div style={
+      {
+        backgroundColor: '#9370DB', 
+        padding: '10px', 
+        color: '#fff',
+        borderRadius: '15px'
+      }}>
+      <h1>Te amo muito meu Amor</h1> 
+      <h2>Você e a pessoa mais linda❤ ️️do mundo!</h2>
+
+      <h3>Ja estou com muita saudade de vc minha princesinha 💕</h3>
+      <p>❤️☺️😍😶‍🌫️😘💕</p>
+    </div>
   )
 }
 export default Home
