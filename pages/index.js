cb7265d98cc54d2c2dfbfcdfff99244cac5ea100
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2025-11-18 11:50:27 +0000

    nova frase

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@
 function Home(){
   return(
     <div>
-      <h1>Eu AMO JESUS CRISTO</h1> 
+      <h1>EU AMO JESUS CRISTO</h1> 
       <h2>Nosso criador e salvador, nele confio</h2>
 
       <h3></h3>
-     
+  
     </div>
   )
 }
