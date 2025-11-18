c4b6d999bfdfb1f7e6aae911e33c7bf3cd2dcf0b
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2025-11-18 11:36:22 +0000

    alteracao de frase n2

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 
 function Home(){
   return(
-    <div style={
-      {
-        backgroundColor: '#9370DB', 
-        padding: '10px', 
-        color: '#fff',
-        borderRadius: '15px'
-      }}>
+    <div>
       <h1>Te amo muito meu Amor</h1> 
-      <h2>Você e a pessoa mais linda❤ ️️do mundo!</h2>
+      <h2>Você e a pessoa mais linda do mundo!</h2>
 
-      <h3>Ja estou com muita saudade de vc minha princesinha 💕</h3>
-      <p>❤️☺️😍😶‍🌫️😘💕</p>
+      <h3>Ja estou com muita saudade de vc minha princesinha</h3>
+     
     </div>
   )
 }
