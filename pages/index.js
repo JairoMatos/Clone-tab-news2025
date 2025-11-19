cba1503bb812d2ca3d5310e7f7c55755454a397c
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2025-11-19 22:06:09 +0000

    altera frase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-
-function Home(){
-  return(
+function Home() {
+  const nome = "ola";
+  if (nome) {
+    console.log(nome);
+  }
+  return (
     <div>
-      <h1>EU AMO JESUS CRISTO</h1> 
+      <h1>EU AMO JESUS CRISTO</h1>
       <h2>Nosso criador e salvador, nele confio</h2>
       <h3>adiciona uma nova frase</h3>
 
       <h3></h3>
-  
     </div>
-  )
+  );
 }
-export default Home
+export default Home;
