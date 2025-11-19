6530be87fd93f89f6a89d8492c32ab6ca654ad43
JairoMatos <jairo.oliveira22@gmail.com>
2025-11-19 19:40:41 -0300

    altera pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 function Home() {
-  const nome = "ola";
-  if (nome) {
-    console.log(nome);
-  }
+  const nome = "Jairo";
+
   return (
     <div>
       <h1>EU AMO JESUS CRISTO</h1>
       <h2>Nosso criador e salvador, nele confio</h2>
-      <h3>adiciona uma nova frase</h3>
+
+      <h3>Meu nome e {nome}</h3>
 
       <h3></h3>
     </div>
