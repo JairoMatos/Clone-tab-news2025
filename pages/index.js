261a365b398ceb5551a4646541802ee3bf188db9
JairoMatos <jairo.oliveira22@gmail.com>
2025-11-21 21:24:33 -0300

    alteracao da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
+import { useState } from "react";
 function Home() {
-  const nome = "Jairo";
-
+  const [num, setNum] = useState(1);
+  function count() {
+    setNum(num + 1);
+  }
   return (
     <div>
       <h1>EU AMO JESUS CRISTO</h1>
       <h2>Nosso criador e salvador, nele confio</h2>
-
-      <h3>Meu nome e {nome}</h3>
-
-      <h3></h3>
+      <button onClick={count}>Contador: {num}</button>
     </div>
   );
 }
