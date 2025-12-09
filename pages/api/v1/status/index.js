c044e7818bca91438ac490ccc18214679bf6e2b5
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2025-12-09 15:32:13 +0000

    atualiza arquivos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 11 + 11;");
+  console.log(result.rows);
   response.status(200).json({
-    Gfoco: {
-      participantes: {
-        1: {
-          nome: "jairo",
-          idade: 39,
-          altura: 185,
-          peso_inicial: 115,
-          peso_atual: 110,
-          peso_alvo: "88",
-          abordagem: "dieta cetogenica",
-          inicio: "10/11/2025",
-        },
-        2: {
-          nome: "ana carolina",
-        },
-        3: {
-          nome: "caina",
-        },
-      },
-    },
+    chave: "valor",
   });
 }
 export default status;
